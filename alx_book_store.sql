8d2b03ede06e468dbe92f8191ef8fe38760ac4f9
Haile Kenasa <142696185+hailekenasatafa@users.noreply.github.com>
2024-10-04 15:03:41 -0400

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,16 +1,11 @@
--- Create the database
-CREATE DATABASE alx_book_store;
-
--- Use the database
+CREATE DATABASE IF NOT EXISTS alx_book_store; 
 USE alx_book_store;
-
--- Create the Authors table
+ 
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
-
--- Create the Books table
+ 
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -19,24 +14,21 @@ CREATE TABLE Books (
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
-
--- Create the Customers table
+ 
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT
 );
-
--- Create the Orders table
+ 
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-
--- Create the Order_Details table
+ 
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
