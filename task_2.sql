64fae5ada7bbc0681b1d1e224d3f826938fc8181
Haile Kenasa <142696185+hailekenasatafa@users.noreply.github.com>
2024-10-04 15:09:28 -0400

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,44 +1,37 @@
--- Select the alx_book_store database
 USE alx_book_store;
 
--- Create authors table
-CREATE TABLE authors (
-    author_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Create books table
-CREATE TABLE books (
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
-    price DOUBLE,
+    price DOUBLE NOT NULL,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
--- Create customers table
-CREATE TABLE customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215),
-    address TEXT
+    email VARCHAR(215) NOT NULL,
+    address TEXT NOT NULL
 );
 
--- Create orders table
-CREATE TABLE orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    order_date DATE NOT NULL, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Create order_details table
-CREATE TABLE order_details (
-    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+CREATE TABLE Order_Details (
+    OrderDetail INT AUTO_INCREMENT PRIMARY KEY,
+    Order_id INT,
+    Book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
